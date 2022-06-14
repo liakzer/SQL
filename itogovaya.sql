c09a3a62081d6e7f54ea40176fc93d833b2496ba
liakzer <bajul@ya.ru>
2022-06-14 19:18:19 +0500

    made some changes

--- itogovaya.sql
+++ itogovaya.sql
@@ -1,41 +1,41 @@
 SET search_path TO bookings;
 
 
---1 В каких городах больше одного аэропорта
+--1 Р’ РєР°РєРёС… РіРѕСЂРѕРґР°С… Р±РѕР»СЊС€Рµ РѕРґРЅРѕРіРѕ Р°СЌСЂРѕРїРѕСЂС‚Р°.
 
 select a.city , count(*) as cnt
 from airports a 
 group by a.city
-having count (*) > 1 --вывела  строки с кол-вотм аэропортов более 1
+having count (*) > 1 --РІС‹РІРµР»Р°  СЃС‚СЂРѕРєРё СЃ РєРѕР»-РІРѕС‚Рј Р°СЌСЂРѕРїРѕСЂС‚РѕРІ Р±РѕР»РµРµ 1
  
 
---2 В каких аэропортах есть рейсы, выполняемые самолетом с максимальной дальностью перелета?
+--2 Р’ РєР°РєРёС… Р°СЌСЂРѕРїРѕСЂС‚Р°С… РµСЃС‚СЊ СЂРµР№СЃС‹, РІС‹РїРѕР»РЅСЏРµРјС‹Рµ СЃР°РјРѕР»РµС‚РѕРј СЃ РјР°РєСЃРёРјР°Р»СЊРЅРѕР№ РґР°Р»СЊРЅРѕСЃС‚СЊСЋ РїРµСЂРµР»РµС‚Р°?
 
   select distinct (a.airport_name), a.city,a.airport_code, t.rannge 
 from (
-		 select aircraft_code, max(ai.range) as rannge -- (1)выбрала 1 макс значение
+		 select aircraft_code, max(ai.range) as rannge -- (1)РІС‹Р±СЂР°Р»Р° 1 РјР°РєСЃ Р·РЅР°С‡РµРЅРёРµ
 		 FROM aircrafts ai
 		 group by ai.aircraft_code
 		 ORDER BY max(ai.range) desc limit 1 --(1)
 		 ) t
- join flights f on f.aircraft_code =t.aircraft_code -- объединила с таблицей  перелеты 
- join airports a on a.airport_code = f.departure_airport  or a.airport_code=f.arrival_airport --обединила с перелеты с аэропортами
+ join flights f on f.aircraft_code =t.aircraft_code -- РѕР±СЉРµРґРёРЅРёР»Р° СЃ С‚Р°Р±Р»РёС†РµР№  РїРµСЂРµР»РµС‚С‹ 
+ join airports a on a.airport_code = f.departure_airport  or a.airport_code=f.arrival_airport --РѕР±РµРґРёРЅРёР»Р° СЃ РїРµСЂРµР»РµС‚С‹ СЃ Р°СЌСЂРѕРїРѕСЂС‚Р°РјРё
  
  
  
- --3 Вывести 10 рейсов с максимальным временем задержки вылета
+ --3 Р’С‹РІРµСЃС‚Рё 10 СЂРµР№СЃРѕРІ СЃ РјР°РєСЃРёРјР°Р»СЊРЅС‹Рј РІСЂРµРјРµРЅРµРј Р·Р°РґРµСЂР¶РєРё РІС‹Р»РµС‚Р°
 --explain analyze
 
  select flight_id , flight_no,  departure_airport, arrival_airport, (actual_departure - scheduled_departure) as delay--, scheduled_departure,actual_departure
  from flights f 
- where actual_departure IS NOT NULL -- отфильтровала по дате вылета 
+ where actual_departure IS NOT NULL -- РѕС‚С„РёР»СЊС‚СЂРѕРІР°Р»Р° РїРѕ РґР°С‚Рµ РІС‹Р»РµС‚Р° 
  ORDER BY delay desc limit 10
  
  
---4 Были ли брони, по которым не были получены посадочные талоны?
+--4 Р‘С‹Р»Рё Р»Рё Р±СЂРѕРЅРё, РїРѕ РєРѕС‚РѕСЂС‹Рј РЅРµ Р±С‹Р»Рё РїРѕР»СѓС‡РµРЅС‹ РїРѕСЃР°РґРѕС‡РЅС‹Рµ С‚Р°Р»РѕРЅС‹?
 -- explain analyze 
 
- select  count(distinct book_ref ) --  отобрала все билеты к которым не были выданы посадочные
+ select  count(distinct book_ref ) --  РѕС‚РѕР±СЂР°Р»Р° РІСЃРµ Р±РёР»РµС‚С‹ Рє РєРѕС‚РѕСЂС‹Рј РЅРµ Р±С‹Р»Рё РІС‹РґР°РЅС‹ РїРѕСЃР°РґРѕС‡РЅС‹Рµ
 from boarding_passes bp 
 right outer join  tickets t2  using (ticket_no)
 where bp.boarding_no is null
@@ -43,64 +43,64 @@ where bp.boarding_no is null
 			
 			
 			
--- 5 Найдите количество свободных мест для каждого рейса, их % отношение к общему количеству мест в самолете.
---Добавьте столбец с накопительным итогом - суммарное накопление количества вывезенных пассажиров из каждого аэропорта на каждый день. 
---Т.е. в этом столбце должна отражаться накопительная сумма - сколько человек уже вылетело из данного аэропорта на этом или более ранних рейсах в течении дня.
+-- 5 РќР°Р№РґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ СЃРІРѕР±РѕРґРЅС‹С… РјРµСЃС‚ РґР»СЏ РєР°Р¶РґРѕРіРѕ СЂРµР№СЃР°, РёС… % РѕС‚РЅРѕС€РµРЅРёРµ Рє РѕР±С‰РµРјСѓ РєРѕР»РёС‡РµСЃС‚РІСѓ РјРµСЃС‚ РІ СЃР°РјРѕР»РµС‚Рµ.
+--Р”РѕР±Р°РІСЊС‚Рµ СЃС‚РѕР»Р±РµС† СЃ РЅР°РєРѕРїРёС‚РµР»СЊРЅС‹Рј РёС‚РѕРіРѕРј - СЃСѓРјРјР°СЂРЅРѕРµ РЅР°РєРѕРїР»РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° РІС‹РІРµР·РµРЅРЅС‹С… РїР°СЃСЃР°Р¶РёСЂРѕРІ РёР· РєР°Р¶РґРѕРіРѕ Р°СЌСЂРѕРїРѕСЂС‚Р° РЅР° РєР°Р¶РґС‹Р№ РґРµРЅСЊ. 
+--Рў.Рµ. РІ СЌС‚РѕРј СЃС‚РѕР»Р±С†Рµ РґРѕР»Р¶РЅР° РѕС‚СЂР°Р¶Р°С‚СЊСЃСЏ РЅР°РєРѕРїРёС‚РµР»СЊРЅР°СЏ СЃСѓРјРјР° - СЃРєРѕР»СЊРєРѕ С‡РµР»РѕРІРµРє СѓР¶Рµ РІС‹Р»РµС‚РµР»Рѕ РёР· РґР°РЅРЅРѕРіРѕ Р°СЌСЂРѕРїРѕСЂС‚Р° РЅР° СЌС‚РѕРј РёР»Рё Р±РѕР»РµРµ СЂР°РЅРЅРёС… СЂРµР№СЃР°С… РІ С‚РµС‡РµРЅРёРё РґРЅСЏ.
 
 with c1 as (
-		select distinct (aircraft_code) , count(seat_no) over (partition by s.aircraft_code) as all_seats -- всего кол-во мест в самолете
+		select distinct (aircraft_code) , count(seat_no) over (partition by s.aircraft_code) as all_seats -- РІСЃРµРіРѕ РєРѕР»-РІРѕ РјРµСЃС‚ РІ СЃР°РјРѕР»РµС‚Рµ
 		from seats s
 		group by aircraft_code, seat_no			
 ), 	c2 as ( 
 		select  distinct (f.flight_id), f.flight_no, f.actual_departure, f.departure_airport , f.aircraft_code, c1.all_seats,
-		count(seat_no) over (partition by f.flight_id order by f.aircraft_code )as book_seat --(1) кол-во занятых мест
+		count(seat_no) over (partition by f.flight_id order by f.aircraft_code )as book_seat --(1) РєРѕР»-РІРѕ Р·Р°РЅСЏС‚С‹С… РјРµСЃС‚
 		from  flights f 
-		join boarding_passes bp using (flight_id)--объединила забронированные места и перелеты
-		join c1 on c1.aircraft_code = f.aircraft_code --присоединили общее кол-во мест в самолете
-		where f.status ilike  'arrived' or status ilike 'Departed' --(1) отбор по вылетам с законченной регистрацией
-		group by f.flight_id, f.flight_no, f.actual_departure, f.departure_airport , f.aircraft_code,bp.seat_no, c1.all_seats -- сгруппировали по столбцам для дальнейших расчетов
+		join boarding_passes bp using (flight_id)--РѕР±СЉРµРґРёРЅРёР»Р° Р·Р°Р±СЂРѕРЅРёСЂРѕРІР°РЅРЅС‹Рµ РјРµСЃС‚Р° Рё РїРµСЂРµР»РµС‚С‹
+		join c1 on c1.aircraft_code = f.aircraft_code --РїСЂРёСЃРѕРµРґРёРЅРёР»Рё РѕР±С‰РµРµ РєРѕР»-РІРѕ РјРµСЃС‚ РІ СЃР°РјРѕР»РµС‚Рµ
+		where f.status ilike  'arrived' or status ilike 'Departed' --(1) РѕС‚Р±РѕСЂ РїРѕ РІС‹Р»РµС‚Р°Рј СЃ Р·Р°РєРѕРЅС‡РµРЅРЅРѕР№ СЂРµРіРёСЃС‚СЂР°С†РёРµР№
+		group by f.flight_id, f.flight_no, f.actual_departure, f.departure_airport , f.aircraft_code,bp.seat_no, c1.all_seats -- СЃРіСЂСѓРїРїРёСЂРѕРІР°Р»Рё РїРѕ СЃС‚РѕР»Р±С†Р°Рј РґР»СЏ РґР°Р»СЊРЅРµР№С€РёС… СЂР°СЃС‡РµС‚РѕРІ
 ) 
  select c2.flight_id, c2.all_seats,c2.book_seat,
- 	(c2.all_seats-c2.book_seat) as free_seats, -- свободные места
- 	(c2.all_seats-c2.book_seat)*100/c2.all_seats   as procent_free, --% отношение свободных мест к общему количеству мест в самолете
- 	sum(book_seat) over (partition by c2.departure_airport, date_trunc('days', c2.actual_departure) order by c2.actual_departure) as YTD_day-- сумма нарастающим итогоу по вылетевшим пассажирам в течение дня из одного аэропрорта,
+ 	(c2.all_seats-c2.book_seat) as free_seats, -- СЃРІРѕР±РѕРґРЅС‹Рµ РјРµСЃС‚Р°
+ 	(c2.all_seats-c2.book_seat)*100/c2.all_seats   as procent_free, --% РѕС‚РЅРѕС€РµРЅРёРµ СЃРІРѕР±РѕРґРЅС‹С… РјРµСЃС‚ Рє РѕР±С‰РµРјСѓ РєРѕР»РёС‡РµСЃС‚РІСѓ РјРµСЃС‚ РІ СЃР°РјРѕР»РµС‚Рµ
+ 	sum(book_seat) over (partition by c2.departure_airport, date_trunc('days', c2.actual_departure) order by c2.actual_departure) as YTD_day-- СЃСѓРјРјР° РЅР°СЂР°СЃС‚Р°СЋС‰РёРј РёС‚РѕРіРѕСѓ РїРѕ РІС‹Р»РµС‚РµРІС€РёРј РїР°СЃСЃР°Р¶РёСЂР°Рј РІ С‚РµС‡РµРЅРёРµ РґРЅСЏ РёР· РѕРґРЅРѕРіРѕ Р°СЌСЂРѕРїСЂРѕСЂС‚Р°,
 from c2
 	
 
 
--- 6 Найдите процентное соотношение перелетов по типам самолетов от общего количества.
+-- 6 РќР°Р№РґРёС‚Рµ РїСЂРѕС†РµРЅС‚РЅРѕРµ СЃРѕРѕС‚РЅРѕС€РµРЅРёРµ РїРµСЂРµР»РµС‚РѕРІ РїРѕ С‚РёРїР°Рј СЃР°РјРѕР»РµС‚РѕРІ РѕС‚ РѕР±С‰РµРіРѕ РєРѕР»РёС‡РµСЃС‚РІР°.
  
 select distinct (f.aircraft_code),
-	count (*) over() as count_reis,--общее кол-во рейсов
-	count(flight_no) over(partition by  f.aircraft_code) as  count_air, --кол-во перелетов каждого самолета
-	cast( ROUND ( count(flight_no) over(partition by  f.aircraft_code)*100.0/count (*) over(),0)as numeric) as procent --процентное перелетов по типам самолетов от общего количества
+	count (*) over() as count_reis,--РѕР±С‰РµРµ РєРѕР»-РІРѕ СЂРµР№СЃРѕРІ
+	count(flight_no) over(partition by  f.aircraft_code) as  count_air, --РєРѕР»-РІРѕ РїРµСЂРµР»РµС‚РѕРІ РєР°Р¶РґРѕРіРѕ СЃР°РјРѕР»РµС‚Р°
+	cast( ROUND ( count(flight_no) over(partition by  f.aircraft_code)*100.0/count (*) over(),0)as numeric) as procent --РїСЂРѕС†РµРЅС‚РЅРѕРµ РїРµСЂРµР»РµС‚РѕРІ РїРѕ С‚РёРїР°Рј СЃР°РјРѕР»РµС‚РѕРІ РѕС‚ РѕР±С‰РµРіРѕ РєРѕР»РёС‡РµСЃС‚РІР°
 from flights f
 
 
 
---7 Были ли города, в которые можно  добраться бизнес - классом дешевле, чем эконом-классом в рамках перелета
+--7 Р‘С‹Р»Рё Р»Рё РіРѕСЂРѕРґР°, РІ РєРѕС‚РѕСЂС‹Рµ РјРѕР¶РЅРѕ  РґРѕР±СЂР°С‚СЊСЃСЏ Р±РёР·РЅРµСЃ - РєР»Р°СЃСЃРѕРј РґРµС€РµРІР»Рµ, С‡РµРј СЌРєРѕРЅРѕРј-РєР»Р°СЃСЃРѕРј РІ СЂР°РјРєР°С… РїРµСЂРµР»РµС‚Р°
 --explain analyse		
 with tc1 as (
-			select  distinct (flight_id) , fare_conditions , amount --отфильтровала билеты эконом класса
+			select  distinct (flight_id) , fare_conditions , amount --РѕС‚С„РёР»СЊС‚СЂРѕРІР°Р»Р° Р±РёР»РµС‚С‹ СЌРєРѕРЅРѕРј РєР»Р°СЃСЃР°
 			from ticket_flights tf 
 			where fare_conditions ilike 'economy'
 	),tc2 as (
 			select distinct(tf.flight_id) , tf.fare_conditions , tf.amount as business_amount, tc1.fare_conditions, tc1.amount as econom_amount
 			from ticket_flights tf 
-			inner join tc1 on tc1.flight_id= tf.flight_id --объединение только по тем перелетам, в которых есть и эконом и бизнесс класс
-			where tf.fare_conditions ilike 'business' -- билеты только бизнесс класса
+			inner join tc1 on tc1.flight_id= tf.flight_id --РѕР±СЉРµРґРёРЅРµРЅРёРµ С‚РѕР»СЊРєРѕ РїРѕ С‚РµРј РїРµСЂРµР»РµС‚Р°Рј, РІ РєРѕС‚РѕСЂС‹С… РµСЃС‚СЊ Рё СЌРєРѕРЅРѕРј Рё Р±РёР·РЅРµСЃСЃ РєР»Р°СЃСЃ
+			where tf.fare_conditions ilike 'business' -- Р±РёР»РµС‚С‹ С‚РѕР»СЊРєРѕ Р±РёР·РЅРµСЃСЃ РєР»Р°СЃСЃР°
 	)
 select fv.flight_id, fv.flight_no, fv.departure_city, fv.arrival_city,tc2.business_amount, tc2.econom_amount
 from flights_v fv
-join tc2 on tc2.flight_id=fv.flight_id --объединила перелеты со стоимостью билетов для вывода списка городов
-where tc2.business_amount < tc2.econom_amount --фитльтрация по условию
+join tc2 on tc2.flight_id=fv.flight_id --РѕР±СЉРµРґРёРЅРёР»Р° РїРµСЂРµР»РµС‚С‹ СЃРѕ СЃС‚РѕРёРјРѕСЃС‚СЊСЋ Р±РёР»РµС‚РѕРІ РґР»СЏ РІС‹РІРѕРґР° СЃРїРёСЃРєР° РіРѕСЂРѕРґРѕРІ
+where tc2.business_amount < tc2.econom_amount --С„РёС‚Р»СЊС‚СЂР°С†РёСЏ РїРѕ СѓСЃР»РѕРІРёСЋ
 
 
 
---8.Между какими городами нет прямых рейсов?
+--8.РњРµР¶РґСѓ РєР°РєРёРјРё РіРѕСЂРѕРґР°РјРё РЅРµС‚ РїСЂСЏРјС‹С… СЂРµР№СЃРѕРІ?
 
 create view aircity as
-select *    -- декартово произведение без cross join
+select *    -- РґРµРєР°СЂС‚РѕРІРѕ РїСЂРѕРёР·РІРµРґРµРЅРёРµ Р±РµР· cross join
 from (
 	select  a.city as depCity, a2.city as arrivCity
 	from airports a,  airports a2
@@ -109,26 +109,26 @@ from (
 	except 
 	select  a.city, a2.city  
 from airports a
-inner join flights f on a.airport_code=f.departure_airport --исключили города отправления, которые есть в расписании
-inner join airports a2 on a2.airport_code=f.arrival_airport -- исключили города прилета, которые есть в расписании
+inner join flights f on a.airport_code=f.departure_airport --РёСЃРєР»СЋС‡РёР»Рё РіРѕСЂРѕРґР° РѕС‚РїСЂР°РІР»РµРЅРёСЏ, РєРѕС‚РѕСЂС‹Рµ РµСЃС‚СЊ РІ СЂР°СЃРїРёСЃР°РЅРёРё
+inner join airports a2 on a2.airport_code=f.arrival_airport -- РёСЃРєР»СЋС‡РёР»Рё РіРѕСЂРѕРґР° РїСЂРёР»РµС‚Р°, РєРѕС‚РѕСЂС‹Рµ РµСЃС‚СЊ РІ СЂР°СЃРїРёСЃР°РЅРёРё
 
 select *, count(*) over() 
 from aircity t
 
 
 
---9.Вычислите расстояние между аэропортами, связанными прямыми рейсами, сравните с допустимой максимальной дальностью перелетов  в самолетах, 
---.обслуживающих эти рейсы
+--9.Р’С‹С‡РёСЃР»РёС‚Рµ СЂР°СЃСЃС‚РѕСЏРЅРёРµ РјРµР¶РґСѓ Р°СЌСЂРѕРїРѕСЂС‚Р°РјРё, СЃРІСЏР·Р°РЅРЅС‹РјРё РїСЂСЏРјС‹РјРё СЂРµР№СЃР°РјРё, СЃСЂР°РІРЅРёС‚Рµ СЃ РґРѕРїСѓСЃС‚РёРјРѕР№ РјР°РєСЃРёРјР°Р»СЊРЅРѕР№ РґР°Р»СЊРЅРѕСЃС‚СЊСЋ РїРµСЂРµР»РµС‚РѕРІ  РІ СЃР°РјРѕР»РµС‚Р°С…, 
+--.РѕР±СЃР»СѓР¶РёРІР°СЋС‰РёС… СЌС‚Рё СЂРµР№СЃС‹
 
 
 select a.city as departure_airport,a2.city as arrival_city,a3.model,a3."range" as air_range,
 	round( acos(sind(a.latitude)*sind(a2.latitude) + cosd(a.latitude)*cosd(a2.latitude)*cosd(a.longitude-a2.longitude))*6371::numeric) as city_distance, 
 		case when round( acos(sind(a.latitude)*sind(a2.latitude) + cosd(a.latitude)*cosd(a2.latitude)*cosd(a.longitude-a2.longitude))*6371::numeric) <= a3."range" 
-			 then 'Долетит' 
-			 else 'Не долетит' 
+			 then 'Р”РѕР»РµС‚РёС‚' 
+			 else 'РќРµ РґРѕР»РµС‚РёС‚' 
 		end  
 FROM airports a JOIN airports a2
- ON a.city <> a2.city  --создала декартового произведение всех возможных маршрутов между городам
-cross join aircrafts a3 --все возможные комбинации самолетов и городов вылетов
+ ON a.city <> a2.city  --СЃРѕР·РґР°Р»Р° РґРµРєР°СЂС‚РѕРІРѕРіРѕ РїСЂРѕРёР·РІРµРґРµРЅРёРµ РІСЃРµС… РІРѕР·РјРѕР¶РЅС‹С… РјР°СЂС€СЂСѓС‚РѕРІ РјРµР¶РґСѓ РіРѕСЂРѕРґР°Рј
+cross join aircrafts a3 --РІСЃРµ РІРѕР·РјРѕР¶РЅС‹Рµ РєРѕРјР±РёРЅР°С†РёРё СЃР°РјРѕР»РµС‚РѕРІ Рё РіРѕСЂРѕРґРѕРІ РІС‹Р»РµС‚РѕРІ
 
 
